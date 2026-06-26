56e6cfcdb2f340e68c7086bfb89afa51fec53176
jgsoto <sotoj9268@gmail.com>
2026-06-26 14:36:29 -0500

    feat(server): create basic express http server

---
+++ server/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static("public"));
+
+io.on("connection", (socket) => {
+    console.log("Usuario conectado:", socket.id);
+
+    socket.on("mensaje", (data) => {
+        console.log("Mensaje recibido:", data);
+
+        io.emit("respuesta", "Servidor dice: Hola cliente 👋");
+    });
+
+    socket.on("disconnect", () => {
+        console.log("Usuario desconectado");
+    });
+});
+
+server.listen(3000, () => {
+    console.log("Servidor en http://localhost:3000");
+});
